252993bf9336ed3a43af48b1bc137c9540780c6e
Kevin Zheng <hzheng4@andrew.cmu.edu>
2023-11-30 20:58:53 -0500

    implemented irregular polygon circle collison

--- sprites.py
+++ sprites.py
@@ -25,39 +25,81 @@ def onAppStart(app):
     # angles measured with Geogebra graphing calculator
     app.angles = [(0, 28.3), (28.3, 81.3), (81.3, 84.5), (84.5, 107), (107, 132.6), (132.6, 185.7), (185.7, 212.2), (212.2, 232.7), (232.7, 237.2), (237.2, 301.7), (301.7, 360)]
 
+    app.projectileX = 0
+    app.projectileY = 0
+    
 def onStep(app):
     app.playerSpriteCounter = (1 + app.playerSpriteCounter) % len(app.playerSprites)
 
 # rationale for irregular circle intersection:
 # 1. determine angle of circle center relative to irregular polygon center (300, 300)
-# 2. Check if the distance between angle center and selected line segment (which will be in a dict mapped to a range of angles)
+# 2. Check if the distance between angle center and selected line segment
 # is less than circle's radius
 # DONE!
 
 # vector calculation using dot product
 # source: https://stackoverflow.com/questions/1211212/how-to-calculate-an-angle-from-three-points
+# only returns 0 to 180 degrees since it's calculating the angle between 2 vectors
 def angleCalc(p1, p2, p3):
-    # p1 is center point
+    p180 = (288, 490)
+
+    # p1 is center point, p2 is first point in app.coordinates
     vectorA = (p1[0] - p2[0], p1[1] - p2[1])
     vectorB = (p1[0] - p3[0], p1[1] - p3[1])
+
     # angle in radians
     angle = math.acos((vectorA[0] * vectorB[0] + vectorA[1] * vectorB[1]) / 
     (math.sqrt(vectorA[0] ** 2 + vectorA[1] ** 2) * math.sqrt(vectorB[0] ** 2 + vectorB[1] ** 2)))
+
     # convert to degrees
     angle *= 180 / math.pi
+
+    # calculated exact point on irregular polygon where angle is 180 with desmos
+    # basically points to the right side of the line formed by the 180 degrees point and p1
+    # will have their angles changed to 180 to 360 degrees
+    if p3[0] >= p180[0] or p3[0] >= p2[0] and p3[1] <= p2[1]:
+        angle = 360 - angle
     return angle
+
+# uses point-line distance formula
+# source: https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+def distancePointToLine(line, point):
+    p0 = point
+    p1 = line[0]
+    p2 = line[1]
+    d = abs((p2[0] - p1[0]) * (p1[1] - p0[1]) - (p1[0] - p0[0]) * (p2[1] - p1[1])) / distance(p1[0], p1[1], p2[0], p2[1]) 
+    return d
 
 def drawPlayerBox(app):
     for i in range(len(app.coordinates)):
         if i < len(app.coordinates) - 1:
             drawLine(app.coordinates[i][0],app.coordinates[i][1],app.coordinates[i+1][0],app.coordinates[i+1][1])
         
+def playerProjectileEnemyCollison(app):
+    size = 25
+    projectile = (app.projectileX, app.projectileY)
+    center = (191.5, 245)
+    for i in range(len(app.angles)):
+        angle = angleCalc(center, app.coordinates[0], projectile)
+        if angle >= app.angles[i][0] and angle <= app.angles[i][1]:
+            print(angle)
+            lineSegment = (app.coordinates[i], app.coordinates[i + 1])
+            if distancePointToLine(lineSegment, projectile) <= size:
+                return True
+    return False
+
+def onMousePress(app, mouseX, mouseY):
+    app.projectileX = mouseX
+    app.projectileY = mouseY
 
 def redrawAll(app):
     sprite = app.playerSprites[app.playerSpriteCounter]
     drawPlayerBox(app)
     # drawImage(sprite, 0, 0)
     drawCircle(191.5, 245, 5)
+    drawCircle(app.projectileX, app.projectileY, 25)
+    drawCircle(288, 490, 5)
+    print(playerProjectileEnemyCollison(app))
 
 def main():
     runApp(width=600, height=600)
